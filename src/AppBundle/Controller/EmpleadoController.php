baa6d1354b5f7de13b532987f60f818cfa5487ff
Polanco08 <jordano_p@hotmail.es>
2015-05-10 00:36:26 +0200

    Agregada entidad Empleado en la API

--- src/AppBundle/Controller/EmpleadoController.php
+++ src/AppBundle/Controller/EmpleadoController.php
@@ -12,5 +12,51 @@ use AppBundle\Entity\Empleado;
  */
 class EmpleadoController extends ApiRestController
 {
+	/**
+	 * @ApiDoc(
+	 *
+	 * )
+	 * @return array
+	 */
+	public function cgetAction()
+	{
+		return $this->cget("Empleado");
+	}//"get_empleados"     [GET] /empleados
 
+
+	/**
+	 * @param Empleado $servicio
+	 * @return Empleado
+	 */
+	public function getAction(Empleado $empleado)
+	{
+		return $empleado;
+	}//"get_empleados"      [GET] /empleados/{id}
+
+
+	public function postAction()
+	{
+		return $this->post("Empleado");
+	}//"new_empleado"     [POST] /empleados
+
+
+	public  function putAction(Empleado $empleado)
+	{
+		$EmpleadoEditado = $this->deserializa('Empleado');
+
+		$empleado->setNombre($EmpleadoEditado->getNombre());
+		$empleado->setApellidos($EmpleadoEditado->getApellidos());
+		$empleado->setEmail($EmpleadoEditado->getEmail());
+		$empleado->setTelefono($EmpleadoEditado->getTelefono());
+		$empleado->setAlertaCitas($EmpleadoEditado->getAlertaCitas());
+
+		return $this->guarda($empleado);
+	}
+
+
+
+	public function deleteAction(Empleado $empleado)
+	{
+		return $this->borra($empleado);
+	}//"delete_empleado"      [DELETE] /empleados/{id}
 }
