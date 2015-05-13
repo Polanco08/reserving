61dea0a67be531a69b4099bb25c5333273adc51f
Polanco08 <jordano_p@hotmail.es>
2015-05-14 00:44:23 +0200

    Integrado Sistema de usuarios

---
+++ src/AppBundle/Entity/Usuario.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * Usuario
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ *
+ */
+class Usuario extends BaseUser
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
