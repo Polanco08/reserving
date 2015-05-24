a02a03fa79172ceebb0fbfd2ef52f9e02c36650f
Polanco08 <jordano_p@hotmail.es>
2015-05-24 21:30:46 +0200

    Completado formulario crear nuevo servicio

--- web/app/servicios/ServiciosController.js
+++ web/app/servicios/ServiciosController.js
@@ -2,12 +2,16 @@
 
 angular.module('Servicios')
     .controller('ServiciosController',
-        ["$scope", "$stateParams", "$state", "$window", "serviciosResource", function($scope, $stateParams,$state,$window, serviciosResource){
+        ["$scope", "$stateParams", "$state", "$window", "serviciosResource", "EmpleadoResource",
+            function($scope, $stateParams,$state,$window, serviciosResource, EmpleadoResource){
 
             // Formulario nuevo servicio
             $scope.duracion =['10','15','30','45','60','75','90'];
             $scope.habilitado_para_reserva = false;
+            $scope.empleados = EmpleadoResource.query();
             $scope.listadoServicios = serviciosResource.query();
+
+                console.log($scope.empleados);
 
             // Nuevo servicio
             $scope.save = function(servicio){
@@ -18,6 +22,7 @@ angular.module('Servicios')
                    s.precio = servicio.precio;
                    s.duracion = servicio.duracion;
                    s.habilitado_para_reserva = servicio.habilitado_para_reserva;
+                   s.empleado = servicio.empleado;
                    s.$save(function(){
                        $scope.listadoServicios.push(s);
                    });
--- web/app/servicios/ServiciosService.js
+++ web/app/servicios/ServiciosService.js
@@ -1,7 +1,9 @@
 (function(){
     'use strict';
 
-    angular.module('Servicios',[])
+    angular.module('Servicios',
+            []
+        )
         .value('_APIURL', 'http://api.reserving.local')
 
         .factory('serviciosResource', ['$resource', '_APIURL', function($resource, _APIURL){
@@ -10,5 +12,11 @@
                 {servicioId:'@id'},
                 {update : {method:'PUT'}}
             );
+        }])
+
+        // Factory Empleados - Gestión de empleados
+        //----------------------------------------------------------------------
+        .factory('EmpleadoResource', ['$resource','_APIURL', function($resource, _APIURL){
+            return $resource(_APIURL + '/empleados/:empleadoId');
         }])
 })();
